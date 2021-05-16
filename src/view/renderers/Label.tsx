dc560704a8d98ab10f2e9d8274ca70cd6f48e956
Mateusz Zieliński <mateusz.mzielinski@gmail.com>
2021-05-16 21:14:10 +0200

    Hide input label if it has "for" attribute

--- src/view/renderers/Label.tsx
+++ src/view/renderers/Label.tsx
@@ -1,39 +1,38 @@
 import React from "react";
-import { renderContext, ComponentProps } from "./utils";
-import { observer } from "mobx-react";
+import {ComponentProps, renderContext} from "./utils";
+import {observer} from "mobx-react";
 import styled from "styled-components";
-import { TextBoxWrapper } from "./TextBox";
-import { RadioButtonWrapper } from "./Radio";
-import Text from "./Text";
-import { Collapsed } from "./Null";
+import {TextBoxContent} from "./TextBox";
+import {RadioButtonWrapper} from "./Radio";
+import {Collapsed} from "./Null";
 
 const LabelWrapper = styled.div`
   display: block;
   margin: 10px 0;
-  
+
   // prettier-ignore
-  & > ${TextBoxWrapper}, 
-  & + ${TextBoxWrapper},
-  & > ${RadioButtonWrapper}, 
-  & + ${RadioButtonWrapper}{
+  & > ${TextBoxContent},
+  & + ${TextBoxContent},
+  & > ${RadioButtonWrapper},
+  & + ${RadioButtonWrapper} {
     margin-top: 5px;
   }
 `;
 
-export default observer(function Label({ node }: ComponentProps) {
-  const render = React.useContext(renderContext);
+export default observer(function Label({node}: ComponentProps) {
+    const render = React.useContext(renderContext);
 
-  if (node.relations.labelContext?.descendants.length) {
-    return (
-      <LabelWrapper>
-        {render(node.relations.labelContext?.descendants)}
-      </LabelWrapper>
-    );
-  }
+    if (node.relations.labelContext?.descendants.length) {
+        return (
+            <LabelWrapper>
+                {render(node.relations.labelContext?.descendants)}
+            </LabelWrapper>
+        );
+    }
 
-  // if (node.relations.labelOf.length > 0) {
-  //   return <Collapsed />;
-  // }
+    if (node.relations.labelOf.length > 0) {
+        return <Collapsed>{render(node.children)}</Collapsed>;
+    }
 
-  return <LabelWrapper>{render(node.children)}</LabelWrapper>;
+    return <LabelWrapper>{render(node.children)}</LabelWrapper>;
 });
--- src/view/renderers/Null.tsx
+++ src/view/renderers/Null.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { renderContext, ComponentProps, borderRadius, isDescendantOf, useFocusable } from "./utils";
-import { observer } from "mobx-react";
-import { BlockTemplate } from "./Block";
-import { NodeElement } from "../../AOM/types";
+import {borderRadius, ComponentProps, isDescendantOf, renderContext, useFocusable} from "./utils";
+import {observer} from "mobx-react";
+import {BlockTemplate} from "./Block";
+import {NodeElement} from "../../AOM/types";
 import styled from "styled-components";
 
-export const Collapsed = styled.span`
+export function Collapsed({children}: { children: React.ReactChild }) {
+    const [isCollapsed, toggleCollapsed] = React.useReducer((state) => !state, true);
+    return (
+        <CollapsedEl isCollapsed={isCollapsed} onClick={toggleCollapsed}>
+            {isCollapsed ? "…" : (children || "(empty)")}
+        </CollapsedEl>
+    );
+}
+
+export const CollapsedEl = styled.span<{ isCollapsed: boolean }>`
   display: inline-block;
   background: #353535;
   color: #888;
@@ -15,7 +24,11 @@ export const Collapsed = styled.span`
   margin-right: 5px;
 
   ::before {
-    content: "[…]";
+    content: "[";
+  }
+
+  ::after {
+    content: "]";
   }
 
   :hover {
@@ -28,61 +41,60 @@ const NewLine = styled.div`
   margin: 10px 0 5px 0;
 `;
 
-function isLabelDescendantOrSiblingOfLabelledNode(label: NodeElement) {
-  const siblings = label.htmlParent?.htmlChildren;
+export function isLabelDescendantOrSiblingOfLabelledNode(label: NodeElement) {
+    const siblings = label.htmlParent?.htmlChildren?.filter(x => x.hasContent)
+    if (!siblings) {
+        return false;
 
-  if (!siblings) {
+    }
+    const index = siblings.indexOf(label);
+
+    for (const labelled of label.relations.labelOf) {
+        if (isDescendantOf(label, labelled)) return true;
+        if (siblings[index - 1] === labelled) return true;
+        if (siblings[index + 1] === labelled) return true;
+    }
+
     return false;
-  }
-
-  const index = siblings.indexOf(label);
-
-  for (const labelled of label.relations.labelOf) {
-    if (isDescendantOf(label, labelled)) return true;
-    if (siblings[index - 1] === labelled) return true;
-    if (siblings[index + 1] === labelled) return true;
-  }
-
-  return false;
 }
 
-export default observer(function Null({ node }: ComponentProps) {
-  const render = React.useContext(renderContext);
-  const content = render(node.children);
-  const [ref, style] = useFocusable(node);
+export default observer(function Null({node}: ComponentProps) {
+    const render = React.useContext(renderContext);
+    const content = render(node.children);
+    const [ref, style] = useFocusable(node);
 
-  if (typeof content === "string" && content.trim() === "") {
-    return null;
-  }
+    if (typeof content === "string" && content.trim() === "") {
+        return null;
+    }
 
-  if (node.htmlTag !== "body" && (node.isFocused || (node.attributes.tabindex as number) >= 0)) {
+    if (node.htmlTag !== "body" && (node.isFocused || (node.attributes.tabindex as number) >= 0)) {
+        return (
+            <BlockTemplate
+                role={`<${node.htmlTag}>`}
+                ref={ref}
+                style={style}
+                color="#666"
+                header={node.hasCustomAccessibleName ? node.accessibleName : undefined}
+            >
+                {content}
+            </BlockTemplate>
+        );
+    }
+
+    if (isLabelDescendantOrSiblingOfLabelledNode(node)) {
+        return <Collapsed>{content}</Collapsed>;
+    }
+
+    if (node.htmlTag === "p") {
+        return <div style={{margin: "10px 0"}}>{content}</div>;
+    }
+
     return (
-      <BlockTemplate
-        role={`<${node.htmlTag}>`}
-        ref={ref}
-        style={style}
-        color="#666"
-        header={node.hasCustomAccessibleName ? node.accessibleName : undefined}
-      >
-        {content}
-      </BlockTemplate>
+        <div data-key={node.key} data-html-tag={node.htmlTag} style={{display: "contents"}}>
+            {node.htmlTag === "label" && <NewLine/>}
+            {!node.isInline && " "}
+            {content}
+            {!node.isInline && " "}
+        </div>
     );
-  }
-
-  if (isLabelDescendantOrSiblingOfLabelledNode(node)) {
-    return <Collapsed />;
-  }
-
-  if (node.htmlTag === "p") {
-    return <div style={{ margin: "10px 0" }}>{content}</div>;
-  }
-
-  return (
-    <div data-key={node.key} data-html-tag={node.htmlTag} style={{ display: "contents" }}>
-      {node.htmlTag === "label" && <NewLine />}
-      {!node.isInline && " "}
-      {content}
-      {!node.isInline && " "}
-    </div>
-  );
 });
