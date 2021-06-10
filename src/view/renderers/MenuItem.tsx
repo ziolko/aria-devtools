6b0fb16b3c9cae1cecdda3aec05a9dc18e9479b7
Mateusz Zieliński <mateusz.mzielinski@gmail.com>
2021-06-10 22:11:56 +0200

    Small improvement in handling menuitems and tabs

--- src/view/renderers/MenuItem.tsx
+++ src/view/renderers/MenuItem.tsx
@@ -1,26 +1,9 @@
-import styled from "styled-components";
 import React from "react";
-import { borderRadius, useFocusable } from "./utils";
-import { renderContext, ComponentProps } from "./utils";
-import { trimStart } from "../../AOM/utils";
 import { observer } from "mobx-react";
-import { HorizontalBlockTemplate } from "./Heading";
-import {ClickableHorizontalBlock} from "./Tab";
+
+import { ComponentProps } from "./utils";
+import { ClickableHorizontalBlock } from "./Tab";
 
 export default observer(function MenuItem({ node }: ComponentProps) {
-  const render = React.useContext(renderContext);
-  const [ref, style] = useFocusable(node);
-
   return <ClickableHorizontalBlock node={node} header={`${node.role} ${node.attributes.ariaLevel ?? ""}`} />
-
-  return (
-    <HorizontalBlockTemplate
-      header={`${node.role} ${node.attributes.ariaLevel ?? ""}`}
-      ref={ref}
-      style={style}
-      color={"#337"}
-    >
-      {node.hasCustomAccessibleName ? node.accessibleName : render(trimStart(node.htmlChildren))}
-    </HorizontalBlockTemplate>
-  );
 });
