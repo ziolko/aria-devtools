8f9fd5eb3b6e212238ab5d0da56f402f5f92e823
Magnus Reyes <me@magnusreyes.com>
2024-01-08 10:52:01 -0500

    Fix where first <details> child may be whitespace

--- src/view/renderers/Details.tsx
+++ src/view/renderers/Details.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { observer } from "mobx-react";
 
 import { ComponentProps, renderContext } from "./utils";
+import { NodeElement } from "../../AOM/types";
 
 export default observer(function Details({ node }: ComponentProps) {
     const render = React.useContext(renderContext);
@@ -9,8 +10,12 @@ export default observer(function Details({ node }: ComponentProps) {
         const content = render(node.children);
         return <div style={{margin: "10px 0"}}>{content}</div>;
     }
-    const first = node.children[0];
-    if (first && first.htmlTag === "summary") {
+    const first = node.htmlChildren.find(
+        (element) =>
+            "htmlTag" in element ||
+            ("text" in element && !element.text.match(/^\s*$/))
+    )
+    if (first && "htmlTag" in first && first.htmlTag === "summary") {
         const content = render(first);
         return <div style={{margin: "10px 0"}}>{content}</div>;
     }
